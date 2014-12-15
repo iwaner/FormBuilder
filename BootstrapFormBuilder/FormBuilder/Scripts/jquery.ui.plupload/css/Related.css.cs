b83a7aad6f81ec515487692aceaf558d1d7a62b6
CodingBaron <superboy.stu@163.com>
2014-12-15 20:23:21 +0800

    upload control jquery
    upload control

---
+++ BootstrapFormBuilder/FormBuilder/Scripts/jquery.ui.plupload/css/Related.css.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace IEMS.Scripts.Jquery.Pulg_in.jquery.ui.plupload.css
+{
+    public partial class Related : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
