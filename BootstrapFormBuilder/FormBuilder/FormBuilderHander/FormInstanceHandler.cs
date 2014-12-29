f63678a39a166c508ed35b8b472dacd3d9c8a1a9
CodingBaron <superboy.stu@163.com>
2014-12-30 00:31:19 +0800

    commit web config for form instance.

--- BootstrapFormBuilder/FormBuilder/FormBuilderHander/FormInstanceHandler.cs
+++ BootstrapFormBuilder/FormBuilder/FormBuilderHander/FormInstanceHandler.cs
@@ -13,13 +13,18 @@ namespace FormBuilder.FormBuilderHander
 
         public void ProcessRequest(HttpContext context)
         {
-            string templateId = context.Request["InstanceId"];
-            if (!string.IsNullOrEmpty(templateId))
+            string templateId = context.Request["templateId"];
+            string instanceId = context.Request["InstanceId"];
+            if (!string.IsNullOrEmpty(instanceId))
             {
                 var instanceBll = new FormInstanceBLL();
-                var instanceMode = instanceBll.GetFormInstance(templateId.ToInt64());
+                var instanceMode = instanceBll.GetFormInstance(instanceId.ToInt64());
             }
-           
+            else
+            {
+                var instanceBll = new FormTemplateBLL();
+                var instanceMode = instanceBll.GetFormTemplate(templateId.ToInt64());
+            }          
         }
     }
 }
